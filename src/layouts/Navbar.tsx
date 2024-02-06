0cc7daedc6a6b6b4aaf17a80c76c0980ed0eb6c6
HakanOZDABAK <hakanozdabak2000@hotmail.com>
2024-02-06 11:31:57 +0300

    Added Drawer

--- src/layouts/Navbar.tsx
+++ src/layouts/Navbar.tsx
@@ -1,41 +1,79 @@
-import AddIcon from '@mui/icons-material/Add';
-import MenuIcon from '@mui/icons-material/Menu';
-import MoreIcon from '@mui/icons-material/MoreVert';
-import SearchIcon from '@mui/icons-material/Search';
-import AppBar from '@mui/material/AppBar';
-import Box from '@mui/material/Box';
-import CssBaseline from '@mui/material/CssBaseline';
-import Fab from '@mui/material/Fab';
-import IconButton from '@mui/material/IconButton';
-import Toolbar from '@mui/material/Toolbar';
-import { styled } from '@mui/material/styles';
-import * as React from 'react';
-
+import AddIcon from "@mui/icons-material/Add";
+import MenuIcon from "@mui/icons-material/Menu";
+import MoreIcon from "@mui/icons-material/MoreVert";
+import SearchIcon from "@mui/icons-material/Search";
+import { Button, Drawer } from "@mui/material";
+import AppBar from "@mui/material/AppBar";
+import Box from "@mui/material/Box";
+import CssBaseline from "@mui/material/CssBaseline";
+import Fab from "@mui/material/Fab";
+import IconButton from "@mui/material/IconButton";
+import Toolbar from "@mui/material/Toolbar";
+import { styled } from "@mui/material/styles";
+import * as React from "react";
 
 const StyledFab = styled(Fab)({
-  position: 'absolute',
+  position: "absolute",
   zIndex: 1,
   top: -30,
   left: 0,
   right: 0,
-  margin: '0 auto',
+  margin: "0 auto",
 });
 
 export default function Navbar() {
+  type Anchor = 'top' | 'left' | 'bottom' | 'right';
+
+  const [state, setState] = React.useState({
+       top: false,
+    left: false,
+    bottom: false,
+    right: false,
+  });
+
+  const toggleDrawer =
+    (anchor: Anchor, open: boolean) =>
+    (event: React.KeyboardEvent | React.MouseEvent) => {
+      if (
+        event.type === "keydown" &&
+        ((event as React.KeyboardEvent).key === "Tab" ||
+          (event as React.KeyboardEvent).key === "Shift")
+      ) {
+        return;
+      }
+
+      setState({ ...state, [anchor]: open });
+    };
+
   return (
     <React.Fragment>
       <CssBaseline />
-      <AppBar position="fixed" color="primary" sx={{ top: 'auto', bottom: 0 }}>
+      <AppBar position="fixed" color="primary" sx={{ top: "auto", bottom: 0 }}>
         <Toolbar>
           <IconButton color="inherit" aria-label="open drawer">
-            <MenuIcon onClick={()=>alert("Lol")} />
+            {(['left'] as const).map((anchor) => (
+              <React.Fragment key={anchor}>
+                <MenuIcon
+                  onClick={
+                    toggleDrawer(anchor, true)
+                  }
+                />
+                <Drawer
+                  anchor={anchor}
+                  open={state[anchor]}
+                  onClose={toggleDrawer(anchor, false)}
+                >
+                  HELLO GUYSSSS
+                </Drawer>
+              </React.Fragment>
+            ))}
           </IconButton>
           <StyledFab color="secondary" aria-label="add">
             <AddIcon />
           </StyledFab>
           <Box sx={{ flexGrow: 1 }} />
           <IconButton color="inherit">
-            <SearchIcon  />
+            <SearchIcon />
           </IconButton>
           <IconButton color="inherit">
             <MoreIcon />
